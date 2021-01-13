ea67ab8e1c96e249fb5a053ce9aabe0cf14a07d1
Júlio Ballot <jrballot@pop-os.localdomain>
2021-01-13 00:20:37 -0300

    adding main.tf

---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "sa-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "poc-eks-terraform-state-bucket"
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  
+#  lifecycle {
+#    prevent_destroy = true
+#  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_locsk" {
+  name         = "poc-eks-terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+#  lifecycle {
+#    prevent_destroy = true
+#  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "poc-eks-terraform-state-bucket"
+    key            = "global/s3/terraform.tfstate"
+    region         = "sa-east-1"
+    dynamodb_table = "poc-eks-terraform-locks"
+    encrypt        = true
+  }
+}
+
