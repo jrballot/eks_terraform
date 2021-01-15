406f8a3597378cd9a8693d5705751a384a413420
Julio Ballot <julio.ballot@gmail.com>
2021-01-15 12:28:41 -0300

    Adding RDS MariaDB Endpoint output information

--- output.tf
+++ output.tf
@@ -6,3 +6,8 @@ output "endpoint" {
 output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.main.certificate_authority[0].data
 }
+
+
+output "rds_mariadb_endpoint" {
+  value = aws_db_instance.default.endpoint
+}
